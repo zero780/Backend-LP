1d837f6b58b637696ff0d5486d06236df4e1c842
Xavier Figueroa <xavierfigueroav@gmail.com>
2020-01-18 14:06:51 -0500

    Change models and make migrations to meet some new requirements.
    Signed-off-by: Xavier Figueroa <xavierfigueroav@gmail.com>

--- miApp/models.py
+++ miApp/models.py
@@ -1,7 +1,48 @@
 from django.db import models
 from django import forms
 
-# Create your models here.
+class Organizer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    username= models.CharField(max_length=30)
+    email= models.EmailField(max_length=30)
+    password = models.CharField(max_length=20)
+    country = models.CharField(max_length=30, default="Ecuador")
+    city = models.CharField(max_length=30)
+    status = models.BooleanField(null=True, default=True)
+
+    def __str__(self):
+        return '%s: %s' % (self.id, self.name)
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=30)
+    modality_choices = [("Presencial", "Presencial"),
+                        ("Semi-Presencial", "Semi-Presencial"),
+                        ("Remota", "Remota")]
+    modality = models.CharField(max_length=50, choices=modality_choices)
+    image = models.ImageField(null=True, upload_to ='miApp/static/images/events')
+    description = models.TextField(null=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    availability = models.BooleanField(null=True, default=True)
+    status = models.BooleanField(null=True, default=True)
+    organizer = models.ForeignKey(Organizer, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return '%s: %s' % (self.id, self.name)
+
+class Event_Stage(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s: %s' % (self.id, self.name)
 
 class Participant(models.Model):
     id=models.AutoField(primary_key=True)
@@ -11,86 +52,42 @@ class Participant(models.Model):
     email= models.EmailField(max_length=30)
     password = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=10)
-    date_of_birth = models.DateField(default=None)
-    age = models.IntegerField(default=None)
-    country = models.CharField(max_length=30, default="Ecuador")
+    date_of_birth = models.DateField()
+    age = models.IntegerField()
+    country = models.CharField(max_length=30, default='Ecuador')
     city = models.CharField(max_length=30)
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(null=True, default=True)
+    notification_subscriptions = models.ManyToManyField(Event_Stage, through='Notification')
 
     def __str__(self):
-        return '%s: %s %s' % (self.id, self.last_names,self.names)
+        return '%s: %s %s' % (self.id, self.last_names, self.names)
 
 class Group(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     join_code = models.CharField(max_length=50)
-    status = models.BooleanField(default=True)
-
-    def __str__(self):
-        return '%s: %s' % (self.id, self.name)
-
-class Organizer(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    username= models.CharField(max_length=30)
-    email= models.EmailField(max_length=30)
-    password = models.CharField(max_length=20)
-    country = models.CharField(max_length=30, default="Ecuador")
-    city = models.CharField(max_length=30)
-    status = models.BooleanField(default=True)
-
-    def __str__(self):
-        return '%s: %s' % (self.id, self.name)
-
-class Event(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=30)
-    modality_choices = [("Presencial","Presencial"),
-                        ("Semi-Presencial","Semi-Presencial"),
-                        ("Remota","Remota"),]
-    modality = models.CharField(max_length=50,choices=modality_choices)
-    image = models.ImageField(blank=True,upload_to ='miApp/static/images/events')
-    description = models.TextField(blank=True)
-    start_date = models.DateTimeField(default=None)
-    end_date = models.DateTimeField(default=None)
-    availability = models.BooleanField(default=True)
-    status = models.BooleanField(default=True)
-    organizer = models.ForeignKey(Organizer,on_delete=models.CASCADE)
-
+    status = models.BooleanField(null=True, default=True)
+    participants = models.ManyToManyField(Participant, through='Membership')
 
     def __str__(self):
         return '%s: %s' % (self.id, self.name)
 
 class Membership(models.Model):
     id = models.AutoField(primary_key=True)
-    leader = models.CharField(max_length=150)
-    creator = models.CharField(max_length=150)
-    participant = models.ForeignKey(Participant,on_delete=models.CASCADE)
-    group = models.ForeignKey(Group,on_delete=models.CASCADE)
-    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s: Participante: %s\nGrupo: %s\nEvento: %s' % (self.id, self.participant,self.group,self.event)
 
-class Event_Stage(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    start_date = models.DateTimeField(default=None)
-    end_date = models.DateTimeField(default=None)
-    event = models.ForeignKey(Event,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return '%s: %s' % (self.id, self.name)
-
 class Event_constraint(models.Model):
     id = models.AutoField(primary_key=True)
-    min_group_size = models.IntegerField(default=None)
-    max_group_size = models.IntegerField(default=None)
-    min_participant_age = models.IntegerField(default=None)
-    max_participant_age = models.IntegerField(default=None)
-    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    min_group_size = models.IntegerField(null=True, default=None)
+    max_group_size = models.IntegerField(null=True, default=None)
+    min_participant_age = models.IntegerField(null=True, default=None)
+    max_participant_age = models.IntegerField(null=True, default=None)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s: Evento: %s' % (self.id, self.event)
@@ -98,16 +95,7 @@ class Event_constraint(models.Model):
 class Notification(models.Model):
     id = models.AutoField(primary_key=True)
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    event_stage = models.ForeignKey(Event_Stage,on_delete=models.CASCADE)
+    event_stage = models.ForeignKey(Event_Stage, on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s: Participante: %s\nEvento: %s' % (self.id, self.participant, self.event_stage)
-
-
-
-
-
-
-
-
-
